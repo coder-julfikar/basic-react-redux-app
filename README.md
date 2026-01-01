1a8fcdda18d0d8e3432f945e8216ba7e5995a9ba
coder-julfikar <julfikar_islam_khan@hotmail.com>
2026-01-01 11:30:57 +0600

    React-Redux logger MW implemented.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ```bash
 npm install redux react-redux
+npm i redux-logger
 ```
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
-import reducer from "./cake/Reducer";
+import { createStore, applyMiddleware } from "redux";
+// import reducer from "./cake/Reducer";
+import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
 
-const store = createStore(rootReducer); //reducer
+const store = createStore(rootReducer, applyMiddleware(logger)); //reducer
 
 export default store;
